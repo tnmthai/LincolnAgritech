dc49111b0279f516c5f927048c81722b3f2c95de
Thai Tran <31507337+tnmthai@users.noreply.github.com>
2023-04-14 15:15:10 +1200

    urewera

--- Home.py
+++ Home.py
@@ -38,8 +38,8 @@ st.markdown(
 row1_col1, row1_col2 = st.columns(2)
 with row1_col1:
     st.image("data/can.gif")
-    st.image("https://github.com/giswqs/data/raw/main/timelapse/las_vegas.gif")
+    
 
 with row1_col2:
-    st.image("https://github.com/giswqs/data/raw/main/timelapse/goes.gif")
-    st.image("https://github.com/giswqs/data/raw/main/timelapse/fire.gif")
+    st.image("data/urewera.gif")
+    

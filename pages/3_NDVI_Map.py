b9b4367a52d52a771407f51fd108a97e650fb973
Thai Tran <31507337+tnmthai@users.noreply.github.com>
2023-05-30 10:43:21 +1200

    Update 3_NDVI_Map.py

--- pages/3_NDVI_Map.py
+++ pages/3_NDVI_Map.py
@@ -121,11 +121,11 @@ with row1_col2:
     sd = st.date_input(
         "Start date",
         date(2022, 1, 1))
-    start_date = sd
+    start_date = date(sd)
     ed = st.date_input(
         "End date",
         default_date_yesterday)
-    end_date = ed
+    end_date = date(ed)
     st.write('Your dates between:', start_date ,' and ', end_date)
 
 if sample_roi != "Uploaded GeoJSON":

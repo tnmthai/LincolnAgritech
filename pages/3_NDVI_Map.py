a22fce7c6e9ea1e6fdf883cb790477f662cb6453
Thai Tran <31507337+tnmthai@users.noreply.github.com>
2023-05-30 13:23:55 +1200

    Update 3_NDVI_Map.py

--- pages/3_NDVI_Map.py
+++ pages/3_NDVI_Map.py
@@ -134,12 +134,6 @@ with row1_col1:
 
 slider = cols1.slider('Select date', min_value=s_date, value=e_date ,max_value=e_date, format=format)    
 
-st.table(pd.DataFrame([[s_date, slider, e_date]],
-                      columns=['start',
-                               'selected',
-                               'end'],
-                      index=['date']))
-
 with row1_col2:
     today = date.today()
 

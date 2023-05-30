f8067e1369bcfa8b70e91e5f9abc46e78a652889
Thai Tran <31507337+tnmthai@users.noreply.github.com>
2023-05-30 16:18:09 +1200

    Update 3_NDVI_Maps.py

--- pages/3_NDVI_Maps.py
+++ pages/3_NDVI_Maps.py
@@ -203,7 +203,7 @@ NDVI_data = ee.ImageCollection('COPERNICUS/S2_SR').filterDate(start_date, end_da
 NDVI_plot = ee.ImageCollection('COPERNICUS/S2_SR').filterDate(start_date, end_date).filterBounds(aoi).filter(ee.Filter.lt("CLOUDY_PIXEL_PERCENTAGE",20)).map(maskCloudAndShadows).map(calculate_ndvi).map(addDate)
 
 image_ids = NDVI_plot.aggregate_array('system:index').getInfo()
-image_ids
+# image_ids
 st.write(image_ids)
 map1.centerObject(aoi)
 try:

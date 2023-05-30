dfb537b0d1e87991e9922ebf2ccfabe4266b50ed
Thai Tran <31507337+tnmthai@users.noreply.github.com>
2023-05-30 14:58:03 +1200

    AA

--- pages/3_NDVI_Maps.py
+++ pages/3_NDVI_Maps.py
@@ -204,7 +204,7 @@ NDVI_plot = ee.ImageCollection('COPERNICUS/S2_SR').filterDate(start_date, end_da
 # ndvi_values = NDVI_plot.sample(scale=10).first().get('NDVI')
 
 # Convert NDVI values to pandas DataFrame
-ndvi_df = geemap.ee_to_pandas(NDVI_data.clip(aoi).select('NDVI'))
+ndvi_df = geemap.ee_to_pandas(selectors=[NDVI_plot, "NDVI"])
 st.line_chart(ndvi_df)
 map1.centerObject(aoi)
 try:

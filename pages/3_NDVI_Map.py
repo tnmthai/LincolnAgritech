1e9d7fd15778213fc71a2b88fa66f531f98a8d39
Thai Tran <31507337+tnmthai@users.noreply.github.com>
2023-05-30 13:56:50 +1200

    Update 3_NDVI_Map.py

--- pages/3_NDVI_Map.py
+++ pages/3_NDVI_Map.py
@@ -168,23 +168,23 @@ with row1_col1:
         end_date = datetime(year, month, last_day).strftime("%Y-%m-%d")
         st.write('Dates between', start_date ,' and ', end_date)
 
-if sample_roi != "Uploaded GeoJSON":
-    gdf = gpd.GeoDataFrame(
-        index=[0], crs=crs, geometry=[landsat_rois[sample_roi]]
-    )
-    # map1.add_gdf(gdf, "ROI")
-    aoi = geemap.gdf_to_ee(gdf, geodesic=False)
-elif sample_roi == "Uploaded GeoJSON":
-    data = st.file_uploader(
-        "Upload a GeoJSON file to use as an ROI. Customize timelapse parameters and then click the Submit button.",
-        type=["geojson", "kml", "zip"],
-    )
-    if data:
-        gdf = uploaded_file_to_gdf(data)
-        st.session_state["aoi"] = geemap.gdf_to_ee(gdf, geodesic=False)    
+    if sample_roi != "Uploaded GeoJSON":
+        gdf = gpd.GeoDataFrame(
+            index=[0], crs=crs, geometry=[landsat_rois[sample_roi]]
+        )
         # map1.add_gdf(gdf, "ROI")
-    else:
-        aoi = ee.FeatureCollection("FAO/GAUL/2015/level1").filter(ee.Filter.eq('ADM1_NAME','Canterbury')).geometry()
+        aoi = geemap.gdf_to_ee(gdf, geodesic=False)
+    elif sample_roi == "Uploaded GeoJSON":
+        data = st.file_uploader(
+            "Upload a GeoJSON file to use as an ROI. Customize timelapse parameters and then click the Submit button.",
+            type=["geojson", "kml", "zip"],
+        )
+        if data:
+            gdf = uploaded_file_to_gdf(data)
+            st.session_state["aoi"] = geemap.gdf_to_ee(gdf, geodesic=False)    
+            # map1.add_gdf(gdf, "ROI")
+        else:
+            aoi = ee.FeatureCollection("FAO/GAUL/2015/level1").filter(ee.Filter.eq('ADM1_NAME','Canterbury')).geometry()
 map1.add_gdf(gdf, "ROI")
 aoi = geemap.gdf_to_ee(gdf, geodesic=False)
 # else:

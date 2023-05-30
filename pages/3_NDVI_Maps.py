a516c2798e685ef73690c952110719c5b8cc19d2
Thai Tran <31507337+tnmthai@users.noreply.github.com>
2023-05-30 14:50:45 +1200

    Update 3_NDVI_Maps.py

--- pages/3_NDVI_Maps.py
+++ pages/3_NDVI_Maps.py
@@ -200,11 +200,11 @@ aoi = geemap.gdf_to_ee(gdf, geodesic=False)
 
     
 NDVI_data = ee.ImageCollection('COPERNICUS/S2_SR').filterDate(start_date, end_date).filterBounds(aoi).filter(ee.Filter.lt("CLOUDY_PIXEL_PERCENTAGE",20)).map(maskCloudAndShadows).map(getNDVI).map(addDate).median()
-NDVI_plot = ee.ImageCollection('COPERNICUS/S2_SR').filterDate(start_date, end_date).filterBounds(aoi).filter(ee.Filter.lt("CLOUDY_PIXEL_PERCENTAGE",20)).map(maskCloudAndShadows).map(calculate_ndvi).map(addDate).mean()
+NDVI_plot = ee.ImageCollection('COPERNICUS/S2_SR').filterDate(start_date, end_date).filterBounds(aoi).filter(ee.Filter.lt("CLOUDY_PIXEL_PERCENTAGE",20)).map(maskCloudAndShadows).map(calculate_ndvi).mean()
 # ndvi_values = NDVI_plot.sample(scale=10).first().get('NDVI')
 
 # Convert NDVI values to pandas DataFrame
-ndvi_df = geemap.ee_to_pandas(NDVI_data)
+ndvi_df = geemap.ee_to_pandas(NDVI_plot)
 st.line_chart(ndvi_df)
 map1.centerObject(aoi)
 try:

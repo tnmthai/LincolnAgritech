caf5dcb7d9538f7ff3f3ba7ac38c1ab2cace5dc3
Thai Tran <31507337+tnmthai@users.noreply.github.com>
2023-05-31 11:23:00 +1200

    Update 3_NDVI_Maps.py

--- pages/3_NDVI_Maps.py
+++ pages/3_NDVI_Maps.py
@@ -209,7 +209,7 @@ with row1_col1:
             start_date = datetime.strptime(ad, "%Y-%m-%d")
  
             next_date = start_date + timedelta(days=1)
-            end_date = next_date.strftime("%Y-%m-%d")
+            end_date = next_date.strftime("%Y-%m-%d")+"T"
             
     #     NDVI_data = ee.ImageCollection('COPERNICUS/S2_SR').filterDate(start_date, end_date).filterBounds(aoi).filter(ee.Filter.lt("CLOUDY_PIXEL_PERCENTAGE",20)).map(maskCloudAndShadows).map(getNDVI).map(addDate).median()
     #     NDVI_plot = ee.ImageCollection('COPERNICUS/S2_SR').filterDate(start_date, end_date).filterBounds(aoi).filter(ee.Filter.lt("CLOUDY_PIXEL_PERCENTAGE",20)).map(maskCloudAndShadows).map(calculate_ndvi).map(addDate)

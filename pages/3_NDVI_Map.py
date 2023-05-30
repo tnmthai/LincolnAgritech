bc1e51b58636c3167a8f1aee101ad0723ad743b2
Thai Tran <31507337+tnmthai@users.noreply.github.com>
2023-05-30 12:30:26 +1200

    Update 3_NDVI_Map.py

--- pages/3_NDVI_Map.py
+++ pages/3_NDVI_Map.py
@@ -133,12 +133,14 @@ with row1_col2:
 
 
     sd = st.date_input(
-        "Start date",
-        date(2022, 1, 1))
+        "Start date", date(2022, 1, 1),
+        max_value= today,
+        )
     # st.write('start:', sd.strftime("%Y-%m-%d") )
     ed = st.date_input(
         "End date",
-        default_date_yesterday)       
+        default_date_yesterday,
+        min_value= date(2015, 6, 23),)       
     
     st.write('Dates between', sd ,' and ', ed)
     start_date = sd.strftime("%Y-%m-%d") + "T" 

b2d5f456381a4978caea70352b460c716e3e055d
Thai Tran <31507337+tnmthai@users.noreply.github.com>
2023-06-07 13:44:10 +1200

    Update 3_NDVI_Maps.py

--- pages/3_NDVI_Maps.py
+++ pages/3_NDVI_Maps.py
@@ -161,7 +161,7 @@ with row1_col1:
     elif sample_roi == "Draw a polygon":
         stdata = st_folium(map1)
         draw = ee.FeatureCollection(map1.st_draw_features(stdata))
-        aoi= geemap.geopandas_to_ee(draw, geodesic=False)  
+        # aoi= geemap.geopandas_to_ee(draw, geodesic=False)  
         map1.add_gdf(draw, "ROI")
     elif sample_roi == "Uploaded GeoJSON":
         data = st.file_uploader(

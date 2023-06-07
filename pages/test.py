8e72d6c7fbb7dff400ed06857bfed8bff3057f70
Thai Tran <31507337+tnmthai@users.noreply.github.com>
2023-06-07 14:01:16 +1200

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -1,39 +1,6 @@
-import ee
-import geemap
 import streamlit as st
+from streamlit_folium import st_folium
+import folium
 
-# Initialize Earth Engine
-ee.Initialize()
-
-# Create a Streamlit app
-st.title("Draw Polygon with geemap and Streamlit")
-
-# Create a map using geemap
-Map = geemap.Map()
-# Map
-
-# Enable drawing control
-# Map.add_draw_control()
-
-# Display the map in Streamlit
-Map.to_streamlit()
-
-# Get the drawn features
-drawn_features = Map.user_roi
-drawn_features
-# Check if any features are drawn
-if drawn_features is not None:
-    # Convert the drawn features to an Earth Engine Geometry object
-    aoi = geemap.geopandas_to_ee(drawn_features, geodesic=False)
-
-    # Add the Earth Engine Geometry object as a layer to the map
-    Map.addLayer(aoi, {}, "Drawn Polygon")
-
-# Display the map with the drawn polygon
-# if st.button('Say hello'):
-#     st.write('Why hello there')
-#     aoi
-#     Map.to_streamlit()
-# else:
-#     st.write('Goodbye')
-
+m = folium.Map()
+stdata = st_folium(m)

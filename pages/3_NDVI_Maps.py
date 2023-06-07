438a329a020a8b1674e641b2006a246bee7a8d32
Thai Tran <31507337+tnmthai@users.noreply.github.com>
2023-06-08 10:57:57 +1200

    Update 3_NDVI_Maps.py

--- pages/3_NDVI_Maps.py
+++ pages/3_NDVI_Maps.py
@@ -297,13 +297,13 @@ if aoi != []:
                 start_date = datetime.strptime(clickdate, "%Y-%m-%d")
                 next_date = start_date + timedelta(days=1)
                 end_date = next_date.strftime("%Y-%m-%d")+"T"
-                st.write('Clicked date:', start_date.format('YYYY-MM-dd') )
+                st.write('Clicked date:', start_date )
                 NDVI_aday = ee.ImageCollection('COPERNICUS/S2_SR').filterDate(start_date, end_date).filterBounds(aoi).filter(ee.Filter.lt("CLOUDY_PIXEL_PERCENTAGE",90)).map(maskCloudAndShadows).map(getNDVI).map(addDate).median()
                 st.session_state["ndviaday"] = map1.addLayer(NDVI_aday.clip(aoi).select('NDVI'), pallete, "NDVI on "+str(clickdate))
 
                 map1.add_legend(title="NDVI", legend_dict=legend_dict)
         except Exception as e:
-            st.error(e)
+            st.error("Please select a day from the graph.")
 
 map1.addLayerControl()
 

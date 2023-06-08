ed42fbe420410ff7ad140ef3601ffa7e6ca3f836
Thai Tran <31507337+tnmthai@users.noreply.github.com>
2023-06-08 17:04:31 +1200

    Update 3_NDVI_Maps.py

--- pages/3_NDVI_Maps.py
+++ pages/3_NDVI_Maps.py
@@ -305,7 +305,7 @@ if aoi != []:
         # # Create a pandas DataFrame from the lists
         # df = pd.DataFrame({'Date': dates, 'NDVI': ndvi_values})
         
-        col1, col2 = st.columns((3, 5))
+        col1, col2 = st.columns((2, 1))
         
         dfz = pd.DataFrame({'PolygonID': polyids, 'Date': datei, 'NDVI': ndviv})
         col2.subheader("NDVI chart")

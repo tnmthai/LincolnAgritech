f140135d30a2d21d09356bd132106403a95c609c
Thai Tran <31507337+tnmthai@users.noreply.github.com>
2023-07-04 16:08:55 +1200

    Update 4_NIR_Map.py

--- pages/4_NIR_Map.py
+++ pages/4_NIR_Map.py
@@ -43,8 +43,8 @@ RGB = st.selectbox(
     )
 values = [item[item.find("(") + 1: item.find(")")] for item in RGB]
 print(values)
-bands = values.split(",")
-print(bands)
+# bands = values.split(",")
+# print(bands)
 
 rgb = ['B8','B4','B3']
 rgbViza = {"min":0.0, "max":0.7,"bands":rgb}

c5ed9d6f9337235b536650e89afef36b6f590d0a
Thai Tran <31507337+tnmthai@users.noreply.github.com>
2023-07-04 15:48:34 +1200

    Update 4_NIR_Map.py

--- pages/4_NIR_Map.py
+++ pages/4_NIR_Map.py
@@ -7,7 +7,17 @@ import geopandas as gpd
 @st.cache_data
 def ee_authenticate(token_name="EARTHENGINE_TOKEN"):
     geemap.ee_initialize(token_name=token_name)
-
+def maskCloudAndShadows(image):
+  cloudProb = image.select('MSK_CLDPRB')
+  snowProb = image.select('MSK_SNWPRB')
+  cloud = cloudProb.lt(5)
+  snow = snowProb.lt(5)
+  scl = image.select('SCL')
+  shadow = scl.eq(3); # 3 = cloud shadow
+  cirrus = scl.eq(10); # 10 = cirrus
+  # Cloud probability less than 5% or cloud shadow classification
+  mask = (cloud.And(snow)).And(cirrus.neq(1)).And(shadow.neq(1))
+  return image.updateMask(mask).divide(10000)
 st.title("NIR Map")
 ee_authenticate(token_name="EARTHENGINE_TOKEN")
 # geemap.ee_initialize()
@@ -19,6 +29,9 @@ rgbViza = {"min":0.0, "max":0.7,"bands":rgb}
 aoi = ee.FeatureCollection("FAO/GAUL/2015/level0").filter(ee.Filter.eq('ADM0_NAME','New Zealand')).geometry()
 se2a = ee.ImageCollection('COPERNICUS/S2_SR').filterDate(startDate,endDate).filterBounds(aoi).filter(
     ee.Filter.lt("CLOUDY_PIXEL_PERCENTAGE",60)).median().divide(10000)#.clip(aoi)
+se2c = ee.ImageCollection('COPERNICUS/S2_SR').filterDate(
+    startDate,endDate).filterBounds(aoi).filter(
+    ee.Filter.lt("CLOUDY_PIXEL_PERCENTAGE",90)).map(maskCloudAndShadows).median().clip(aoi)
 Map.centerObject(aoi)
-Map.addLayer(se2a, rgbViza, "S2 original")
+Map.addLayer(se2c, rgbViza, "Sentinel 2")
 Map.to_streamlit(height=700)

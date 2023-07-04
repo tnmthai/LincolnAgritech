eefd1b156101b52aab1bc956d3b4dd5cbe81902d
Thai Tran <31507337+tnmthai@users.noreply.github.com>
2023-07-04 14:26:00 +1200

    Update 3_NDVI_Maps.py

--- pages/3_NDVI_Maps.py
+++ pages/3_NDVI_Maps.py
@@ -246,7 +246,7 @@ if aoi != []:
         map1.centerObject(aoi)
         st.session_state["ndvi"] = map1.addLayer(NDVI_data.clip(aoi).select('NDVI'), vis_params, "Median of NDVI for all selected dates")
         
-        map1.add_colormap(vis_params,width=12, height=0.7, label="NDVI", orientation="vertical", layer_name="Median of NDVI for all selected dates", transparent_bg=True)                       
+        map1.add_colormap(vis_params,label="NDVI", orientation="vertical", layer_name="Median of NDVI for all selected dates", transparent_bg=True)                       
     except Exception as e:
         st.error(e)
         st.error("Cloud is greater than 90% on selected day. Please select additional dates!")

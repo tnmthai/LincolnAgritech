cc7bd54e94502f2f5ee3634edd5ffee0c40ac3a1
Thai Tran <31507337+tnmthai@users.noreply.github.com>
2023-07-11 13:12:20 +1200

    Update 2_Index_Maps.py

--- pages/2_Index_Maps.py
+++ pages/2_Index_Maps.py
@@ -241,9 +241,9 @@ with row1_col1:
                 start_date = datetime.strptime(ad, "%Y-%m-%d")    
                 next_date = start_date + timedelta(days=1)
                 end_date = next_date #.strftime("%Y-%m-%d")+"T"
-    # tb = 'Selected dates between '+ start_date +' and '+ end_date    
+    tb = 'Selected dates between '+ str(start_date) +' and '+ str(end_date)
     st.write(start_date, end_date)
-    # st.warning(tb,icon="ℹ️")
+    st.warning(tb,icon="ℹ️")
 
                
 if aoi != []:

a3ef4774b320dbf478dceae141495112f302db2f
Thai Tran <31507337+tnmthai@users.noreply.github.com>
2023-07-12 15:50:32 +1200

    Update 2_Index_Maps.py

--- pages/2_Index_Maps.py
+++ pages/2_Index_Maps.py
@@ -656,7 +656,7 @@ if aoi != []:
         try:
             map1.centerObject(aoi)
             st.session_state["lai"] = map1.addLayer(NDMI_data, vis_params1, "Median of LAI for all selected dates")        
-            map1.add_colormap(width=10, height=0.1, vmin=-7, vmax=7,vis_params= vis_params1,label="LAI", position=(0, 0))
+            # map1.add_colormap(width=10, height=0.1, vmin=-7, vmax=7,vis_params= vis_params1,label="LAI", position=(0, 0))
         except Exception as e:
             st.error(e)
             st.error("Cloud is greater than 90% on selected day. Please select additional dates!")

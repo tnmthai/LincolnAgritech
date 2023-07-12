7bbbbd0043dc364dec39c7449ccdedbe82877bed
Thai Tran <31507337+tnmthai@users.noreply.github.com>
2023-07-12 16:04:57 +1200

    Update 2_Index_Maps.py

--- pages/2_Index_Maps.py
+++ pages/2_Index_Maps.py
@@ -633,7 +633,7 @@ if aoi != []:
         # st.write('Selected dates between:', start_date ,' and ', end_date)
         NDMI_data = ee.ImageCollection('JAXA/GCOM-C/L3/LAND/LAI/V3') \
                 .filterDate(start_date, end_date).filterBounds(aoi) \
-                .filter(ee.Filter.eq('SATELLITE_DIRECTION', 'D')).mean().multiply(0.001).clip(aoi)
+                .filter(ee.Filter.eq('SATELLITE_DIRECTION', 'D')).mean().multiply(0.001)
         
         NDMI_plot = ee.ImageCollection('JAXA/GCOM-C/L3/LAND/LAI/V3') \
                 .filterDate(start_date, end_date).filterBounds(aoi) \
@@ -657,7 +657,7 @@ if aoi != []:
         try:
             map1.centerObject(aoi)
             # st.session_state["lai"] = 
-            map1.addLayer(dataset, vis_params1, "Median of LAI for all selected dates")        
+            map1.addLayer(dataset.clip(aoi), vis_params1, "Median of LAI for all selected dates")        
             map1.add_colormap(width=10, height= 0.1, vmin=-7, vmax=7,vis_params= vis_params1,label="LAI", position=(0, 0))
         except Exception as e:
             st.error(e)

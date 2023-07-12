ff5db9446cb434053e1c4b7e1d24e32e4925e224
Thai Tran <31507337+tnmthai@users.noreply.github.com>
2023-07-12 16:18:46 +1200

    Update 4_Split_Maps.py

--- pages/4_Split_Maps.py
+++ pages/4_Split_Maps.py
@@ -1,17 +1,64 @@
 import streamlit as st
 import leafmap.foliumap as leafmap
+import ee
+import tempfile
+import os
+import uuid
+import streamlit as st
+import apps.lal as lal
+import geemap.foliumap as geemap
+import geemap as gm
+import ee
+import geopandas as gpd
+from datetime import date, timedelta, datetime
+import fiona
 
 st.set_page_config(layout="wide")
-
-
 st.title("Split-panel Map")
+def maskCloudAndShadows(image):
+  cloudProb = image.select('MSK_CLDPRB')
+  snowProb = image.select('MSK_SNWPRB')
+  cloud = cloudProb.lt(5)
+  snow = snowProb.lt(5)
+  scl = image.select('SCL')
+  shadow = scl.eq(3); # 3 = cloud shadow
+  cirrus = scl.eq(10); # 10 = cirrus
+  # Cloud probability less than 5% or cloud shadow classification
+  mask = (cloud.And(snow)).And(cirrus.neq(1)).And(shadow.neq(1))
+  return image.updateMask(mask).divide(10000)
 
-with st.expander("See source code"):
-    with st.echo():
-        m = leafmap.Map(center=(-43.525650, 172.639847), zoom=6.25)
-        m.split_map(
-            left_layer='ESA WorldCover 2020 S2 FCC', right_layer='ESA WorldCover 2020'
-        )
-        m.add_legend(title='ESA Land Cover', builtin_legend='ESA_WorldCover')
+bandRGB = ['B4','B3','B2']
+bandNIR = ['B8','B4','B3']
+
+row1_col1, row1_col2, row1_col3 = st.columns([1, 1 , 1])
+with row1_col2:
+    today = date.today()
+    default_date_yesterday = today - timedelta(days=1)
+
+    sd = st.date_input(
+            "Start date", date(2023, 1, 1), min_value= date(2015, 6, 23),
+            max_value= today,
+            )
+    ed = st.date_input(
+        "End date",
+        default_date_yesterday,
+        min_value= date(2015, 6, 23),max_value= today)       
+    startDate = sd.strftime("%Y-%m-%d") + "T" 
+    endDate = ed.strftime("%Y-%m-%d") + "T" 
+
+    rgbViza = {"min":0.0, "max":0.7,"bands":bandRGB}
+
+se2a = ee.ImageCollection('COPERNICUS/S2_SR').filterDate(startDate,endDate).filter(
+    ee.Filter.lt("CLOUDY_PIXEL_PERCENTAGE",60)).median().divide(10000)
+se2c = ee.ImageCollection('COPERNICUS/S2_SR').filterDate(
+    startDate,endDate).filter(
+    ee.Filter.lt("CLOUDY_PIXEL_PERCENTAGE",90)).map(maskCloudAndShadows).median()
+
+m = leafmap.Map(center=(-43.525650, 172.639847), zoom=6.25)
+m.split_map(
+    left_layer= se2a, right_layer=se2c
+)
+m.add_legend(title='RGB and NIR', legend=rgbViza)
+
 
 m.to_streamlit(height=700)

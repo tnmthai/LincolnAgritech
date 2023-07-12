641b443f206cec7be477f3c8e2d929823024702f
Thai Tran <31507337+tnmthai@users.noreply.github.com>
2023-07-12 14:15:31 +1200

    Update 2_Index_Maps.py

--- pages/2_Index_Maps.py
+++ pages/2_Index_Maps.py
@@ -195,7 +195,7 @@ with row1_col1:
     )
     if sample_roi != "Uploaded GeoJSON":
         gdf = gpd.GeoDataFrame(
-            index=[0], crs=crs, geometry=[roi_options[sample_roi]]
+            index=[0], crs=crs, geometry=[lal.nz_rois[sample_roi]]
         )
         # map1.add_gdf(gdf, "ROI")
         aoi = geemap.gdf_to_ee(gdf, geodesic=False)

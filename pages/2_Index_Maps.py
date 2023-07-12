bdac8f79c4a06c104235593ef0937815de11ec60
Thai Tran <31507337+tnmthai@users.noreply.github.com>
2023-07-12 14:16:33 +1200

    Update 2_Index_Maps.py

--- pages/2_Index_Maps.py
+++ pages/2_Index_Maps.py
@@ -8,7 +8,6 @@ import warnings
 import fiona
 import geopandas as gpd
 from datetime import date, timedelta, datetime
-# import datetime
 from dateutil.rrule import rrule, MONTHLY
 from dateutil.relativedelta import relativedelta # to add days or years
 import pandas as pd
@@ -122,35 +121,6 @@ map1 = geemap.Map(
     # locate_control=True,
     plugin_LatLngPopup=False, center=(-43.525650, 172.639847), zoom=6.25,
 )
-
-# # Load shp files
-# shp = gpd.read_file("data/nzshp/Canterbury.shp")
-# gdf = shp.to_crs({'init': 'epsg:4326'}) 
-# can = []
-# for index, row in gdf.iterrows():
-#     for pt in list(row['geometry'].exterior.coords): 
-#         can.append(list(pt))
-
-# shp = gpd.read_file("data/nzshp/Mitimiti.shp")
-# gdf = shp.to_crs({'init': 'epsg:4326'}) 
-# Mitimiti = []
-# for index, row in gdf.iterrows():
-#     for pt in list(row['geometry'].exterior.coords): 
-#         Mitimiti.append(list(pt))
-
-# shp = gpd.read_file("data/nzshp/Urewera.shp")
-# gdf = shp.to_crs({'init': 'epsg:4326'}) 
-# Urewera = []
-# for index, row in gdf.iterrows():
-#     for pt in list(row['geometry'].exterior.coords): 
-#         Urewera.append(list(pt))
-
-# landsat_rois = {
-#     "Canterbury":Polygon (can),
-#     "Mitimiti": Polygon(  Mitimiti  ),
-#     "Te Urewera": Polygon(  Urewera  ),
-
-# }
 
 roi_options = ["Uploaded GeoJSON"] + list(lal.nz_rois.keys())
 crs = "epsg:4326"

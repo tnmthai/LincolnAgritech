bd99143a69d51531c308e69d6740035b517fda0d
Thai Tran <31507337+tnmthai@users.noreply.github.com>
2023-10-17 14:31:08 +1300

    Update 4_Split_Maps.py

--- pages/4_Split_Maps.py
+++ pages/4_Split_Maps.py
@@ -42,6 +42,9 @@ def addDate(image):
 
 bandRGB = ['B4','B3','B2']
 bandNIR = ['B8','B4','B3']
+palette = ['FFFFFF', 'CE7E45', 'DF923D', 'F1B555', 'FCD163', '99B718', '74A901', '66A000', '529400', '3E8601', '207401', '056201', '004C00', '023B01', '012E01', '011D01', '011301']
+# cm.palettes.ndvi
+
 vis_params = {
   'min': 0,
   'max': 1,
